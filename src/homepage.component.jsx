6f4099ce5e01d45ef9657fce99b4ef47bdb21c88
Dylan Young <youngx@dylans-air.home>
2019-12-13 13:44:10 -0500

    created inital project using create react app

---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="homepage">
+       <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Hats</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Jackets</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Sneakers</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Women's</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Men's</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+       </div>
+    </div>
+);
+
+export default HomePage;
