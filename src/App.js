ce8b4af065238dc5c743a0bff2d110efaad49133
Dylan Young <youngx@dylans-air.home>
2019-12-14 04:19:28 -0500

    update mneu item to navigate withRoute HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+
 import HomePage from './components/homepage/homepage.component';
 
 import './App.css';
 import './components/homepage/component.style.scss';
 
+const JacketsPage = () => (
+  <div>
+       <h1>Awesome new Featured jackets</h1>
+       <h3>Shop the hottest trends today</h3>
+  </div>
+);
+
+const HatsPage = () => (
+   <div>
+     <h3> this is the hats page werer all of our hats that are currently availbialbe will be displayed</h3>
+   </div>
+);
 function App() {
   return (
      <div>
-         <HomePage />
+       <Switch>
+         <Route exact path="/" component={HomePage} />
+         <Route path="/hats" component={HatsPage} />
+         <Route path="/jackets" component={JacketsPage} />
+       </Switch>
      </div>
   );
 }
